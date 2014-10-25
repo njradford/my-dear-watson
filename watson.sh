adb6e5f7d1bc0016514b0dc3057f4a83a3d1e012
nicholas <nicktheradford@gmail.com>
2014-10-24 22:36:07 -0400

    Initial commit. Functional in installation process

---
+++ watson.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+
+chroot () {
+	#Chroot specific functions that are fun from within precise when this script is called with an argument of "c"
+	#------------------------------------------------------------------
+	apt-get install -y python-software-properties
+	add-apt-repository -y ppa:elementary-os/stable
+	apt-get update
+	apt-get install -y elementary-desktop
+	apt-get install -y gtk2-engines-pixbuf
+	
+	cd /usr/bin 
+	cp startxfce4 startelementary
+	sed -i 's/\/etc\/xdg\/xfce4\/xinitrc $CLIENTRC $SERVERRC/\/usr\/bin\/xinit_pantheon/' startelementary
+	echo '#!/bin/sh
+	/usr/sbin/lightdm-session "gnome-session --session=pantheon"' >> xinit_pantheon 
+	chmod +x xinit_pantheon 
+	sudo chown root:root xinit_pantheon
+	sudo apt-get purge xfce4 xfce4-goodies xfce* shimmer-themes netsurf-gtk empathy telepathy indicator-messages brasero geary maya-calendar elementary-scan shotwell totem onboard gnome-orca espeak
+	sudo apt-get auto remove
+}
+
+chrome () {
+	sh -e crouton -t xfce,keyboard,extension
+	sudo enter-chroot -u root sh ~/Downloads/watson.sh c
+	cd /usr/local/bin
+	sudo cp startxfce4 startelementary
+	sudo sed -i 's/startxfce4/startelementary/' startelementary
+	sudo startelementary	
+}
+
+
+
+if [ "$1" = "c" ]
+then chroot
+else chrome
+fi
